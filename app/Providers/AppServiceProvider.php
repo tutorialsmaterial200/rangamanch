26289ef3aa5eaaae2b7e6b374704228497c63362
tutorialsmaterial200 <tutorialsmaterials@gmail.com>
2025-11-06 09:39:50 +0545

    Fix: AppServiceProvider graceful handling of missing settings table during migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Setting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,14 +25,21 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        $setting = Setting::pluck('value', 'key')->toArray();
+        try {
+            $setting = Setting::pluck('value', 'key')->toArray();
 
-        View::composer('*', function($view) use ($setting){
-            $view->with('settings', $setting);
-        });
+            View::composer('*', function($view) use ($setting){
+                $view->with('settings', $setting);
+            });
+        } catch (\Exception $e) {
+            // Handle case when database tables haven't been migrated yet
+            View::composer('*', function($view){
+                $view->with('settings', []);
+            });
+        }
 
         // Add Blade directive for Nepali numerals
-        \Blade::directive('nepaliNum', function ($expression) {
+        Blade::directive('nepaliNum', function ($expression) {
             return "<?php echo convertToNepaliNumerals($expression); ?>";
         });
     }
